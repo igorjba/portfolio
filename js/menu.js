e70479d2224119572f9ec0270ee0676afd79f29b
Igor Jardim Bahia <igorjbahia@gmai.com>
2023-03-13 09:15:41 -0300

    :sparkles: feat: Criação de menu mobile. Conclusão das responsividade.

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+const btnMobile = document.querySelector('.btn-menu-mobile');
+const themeHeader = document.querySelector('#headerTheme');
+const navMenu = document.querySelector('.menu');
+const header = document.querySelector('header');
+
+function toggeMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault(); {
+        header.classList.toggle('menu-active');
+        event.currentTarget.setAttribute('aria-expanded', event.currentTarget.getAttribute('aria-expanded') === 'true' ? 'false' : 'true');
+
+        if (active) {
+            event.currentTarget.setAttribute('aria-label', 'Fechar menu');
+        } else {
+            event.currentTarget.setAttribute('aria-label', 'Abrir menu');
+        }
+
+    }
+}
+
+btnMobile.addEventListener('click', toggeMenu)
+btnMobile.addEventListener('touchstart', toggeMenu)
